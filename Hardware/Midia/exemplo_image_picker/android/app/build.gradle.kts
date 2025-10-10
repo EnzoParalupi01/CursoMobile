29ba9682b87723d806ba1d96ab3e4e767a11ba03
EnzoParalupi01 <enzoparalupi07@gmail.com>
2025-10-10 11:30:28 -0300

    10.10.2025

--- Hardware/Midia/exemplo_image_picker/android/app/build.gradle.kts
+++ Hardware/Midia/exemplo_image_picker/android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sa_geolocator_maps"
-    compileSdk = 35
+    namespace = "com.example.exemplo_image_picker"
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sa_geolocator_maps"
+        applicationId = "com.example.exemplo_image_picker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ Hardware/sa02_geolocator_image/lib/models/photo.dart
@@ -0,0 +1,12 @@
+class Photo {
+  //atributos
+  final String location;
+  final String dateTime;
+  final String photoPath;
+
+  // construtor
+  Photo({
+    required this.location, required this.dateTime, required this.photoPath
+  });
+  
+}
